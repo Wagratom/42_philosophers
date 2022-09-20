ab702f9f9a5886bb1bde90d85718187db615dc4c
wallas <wagratom111@gmail.com>
2022-09-20 14:35:20 -0300

    s

--- sourcs/new_thread.c
+++ sourcs/new_thread.c
@@ -6,19 +6,19 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 14:55:01 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/09/19 13:08:29 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/09/20 14:34:58 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philosophers.h>
 
-t_bool	new_thread(pthread_t *thread, void *(*add_me)(void *))
+t_bool	new_thread(pthread_t *thread, void *(*start)(void *))
 {
-	if (add_me == NULL || *add_me == NULL)
+	if (start == NULL || *start == NULL)
 		return (FALSE);
 	if (thread == NULL)
 		return (FALSE);
-	if (pthread_create(thread, NULL, add_me, "running") != 0)
+	if (pthread_create(thread, NULL, start, "running") != 0)
 	{
 		printf("%s\n", "Erro creating thread\n");
 		return (FALSE);
