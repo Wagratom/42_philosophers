5250f9a0335f1e218c6169d5182edf656d3c3b2a
wallas <wagratom111@gmail.com>
2022-09-21 20:08:06 -0300

    indo pra academia

--- sourcs/philo_eat.c
+++ sourcs/philo_eat.c
@@ -6,7 +6,7 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:38:35 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/09/21 19:04:34 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/09/21 20:03:11 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,10 @@ t_bool	philo_eat(t_table *table, pthread_mutex_t *mutex)
 		return (FALSE);
 	if (pthread_mutex_lock(mutex) != 0)
 		return (FALSE);
+	printf("lock\n");
+	usleep(table->time_eat);
+	if (pthread_mutex_unlock(mutex) != 0)
+		return (FALSE);
+	printf("unlock\n");
 	return (TRUE);
 }
--- test/philo_eat.c
+++ test/philo_eat.c
@@ -6,7 +6,7 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:14:25 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/09/21 19:54:41 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/09/21 20:07:41 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,25 @@ void	test_teardown(void)
 {
 }
 
-MU_TEST(template_tst)
+MU_TEST(philo_eat_tst)
 {
 	pthread_mutex_t		mutex;
 	t_table				*table;
 
 	if (pthread_mutex_init(&mutex, NULL) != 0)
 		printf("return error\n");
-	table = creat_table((char *[]){"10","10", "10", "10", "10", "10", NULL});
+	table = creat_table((char *[]){"10","10", "10", "1000000", "10", "10", NULL});
+	mu_assert_int_eq(TRUE, philo_eat(table, &mutex));
+}
+
+MU_TEST(wait_thread_tst)
+{
+	pthread_mutex_t		mutex;
+	t_table				*table;
+
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+		printf("return error\n");
+	table = creat_table((char *[]){"10","10", "10", "1000000", "10", "10", NULL});
 	mu_assert_int_eq(TRUE, philo_eat(table, &mutex));
 }
 
@@ -36,7 +47,8 @@ MU_TEST_SUITE(test_suite)
 {
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 
-	MU_RUN_TEST(template_tst);
+	MU_RUN_TEST(philo_eat_tst);
+	MU_RUN_TEST(wait_thread_tst);
 }
 
 MU_MAIN
